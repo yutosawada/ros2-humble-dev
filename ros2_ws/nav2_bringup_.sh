6a9e297f7f078ad38a7e12eac934e85639336d31
yutosawada <yu.sawada@fuji.co.jp>
2025-06-24 10:30:51 +0900

    edit docker compose

---
+++ ros2_ws/nav2_bringup_.sh
@@ -0,0 +1,4 @@
+ros2 launch nav2_bringup bringup_launch.py \
+  map:=/ros2_ws/map/map.yaml\
+  use_sim_time:=false autostart:=true \
+  params_file:=/ros2_ws/install/nav2_bringup/share/nav2_bringup/params/nav2_params.yaml
